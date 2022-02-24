e44cd625c5fa49028bf9128092624e63f8d188f6
tatianemoraes <tatiane_213@hotmail.com>
2022-02-23 23:26:18 -0300

    an application of todo list

---
+++ src/about/about.jsx
@@ -0,0 +1,21 @@
+import  React from 'react';
+import PageHeader from '../template/PageHeader';
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit natus ad eveniet inventore quia aliquam eum voluptas totam, laudantium temporibus cupiditate, error quibusdam! Quae reprehenderit cum eaque nulla distinctio!
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati beatae tempore fuga quidem ipsa consectetur architecto accusamus modi voluptate hic, sint animi alias voluptas, libero doloribus? Porro, adipisci nobis. A.
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, doloribus est. Nesciunt ipsa qui ab totam, laborum blanditiis maiores corporis autem id natus modi quia minus voluptates reprehenderit itaque voluptatum!
+    </p>
+  </div>
+);
---
+++ src/todo/TodoList.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import IconButton from '../template/IconButton';
+
+export default props => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+        <td>
+          <IconButton
+            hide={todo.done}
+            style='success' 
+            icon='check' 
+            onClick={() => props.handleMarkAsDone(todo)}
+          >
+          </IconButton>
+          <IconButton 
+            hide={!todo.done}
+            style='warning' 
+            icon='undo' 
+            onClick={() => props.handleMarkAsPending(todo)}
+          >
+          </IconButton>
+          <IconButton 
+            hide={!todo.done}
+            style='danger' 
+            icon='trash-o' 
+            onClick={() => props.handleRemove(todo)}
+          >
+          </IconButton>
+        </td>
+      </tr>
+    ))
+  }
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th className='tableAction'>Ações</th>
+          <tbody>{renderRows()}</tbody>
+        </tr>
+      </thead>
+    </table>
+  )
+};
